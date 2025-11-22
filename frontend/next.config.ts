25aecc236696cde09ef5f9e5392557846de2c21c
gsaintskad <gsaintskad@gmail.com>
2025-11-22 16:29:27 +0100

    next.config edited

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Add this rewrites block
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*", // Matches all requests to /api/
-        destination: "http://localhost:8080/api/:path*", // Proxies them to your PHP API
-      },
-    ];
-  },
+  basePath: "/reglament-sytem",
+  // // Add this rewrites block
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*", // Matches all requests to /api/
+  //       destination: "http://localhost:8080/api/:path*", // Proxies them to your PHP API
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
